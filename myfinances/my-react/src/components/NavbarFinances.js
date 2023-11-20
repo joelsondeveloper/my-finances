e8ac247cc80aff2a514602a7e628208a614e90af
joelsondeveloper <115474776+joelsondeveloper@users.noreply.github.com>
2023-11-20 05:12:38 -0300

    outros reparos

--- myfinances/my-react/src/components/NavbarFinances.js
+++ myfinances/my-react/src/components/NavbarFinances.js
@@ -1,13 +1,30 @@
-import styles from './NavbarFinances.module.css'
+import React from 'react';
+import styles from './NavbarFinances.module.css';
 
 function NavbarFinances() {
     return (
-        <div className='nav'>
-            <img className='logo'  height='60vh' width='100vw'></img>
-
-
+        <div className={styles.nav}>
+            <div className={styles.logo}></div>
+            <div className={styles.linknav}>
+                <h3>Para Você</h3>
+                <div className={styles.baixo}></div>
+            </div>
+            <div className={styles.linknav}>
+                <h3>Para seu negocio</h3>
+                <div className={styles.baixo}></div>
+            </div>
+            <div className={styles.linknav}>
+                <h3>O MyFinances</h3>
+                <div className={styles.baixo}></div>
+            </div>
+            <div className={styles.linknav}>
+                <h3>duvidas frequentes</h3>
+                <div className={styles.baixo}></div>
+            </div>
+            <a href='#'><button className={styles.cadastro}><h3>Cadastre-se</h3></button></a>
+            <div className={styles.logomenu}></div>
         </div>
-    )
+    );
 }
 
-export default NavbarFinances
+export default NavbarFinances;
