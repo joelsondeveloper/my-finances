b48e6dec30e63331b907133555dbce20af297ee0
joelsondeveloper <115474776+joelsondeveloper@users.noreply.github.com>
2023-11-21 11:48:17 -0300

    adicionando dropdown

--- myfinances/my-react/src/components/NavbarFinances.js
+++ myfinances/my-react/src/components/NavbarFinances.js
@@ -1,30 +1,52 @@
 import React from 'react';
 import styles from './NavbarFinances.module.css';
 
-function NavbarFinances() {
-    return (
-        <div className={styles.nav}>
-            <div className={styles.logo}></div>
-            <div className={styles.linknav}>
-                <h3>Para Você</h3>
-                <div className={styles.baixo}></div>
-            </div>
-            <div className={styles.linknav}>
-                <h3>Para seu negocio</h3>
-                <div className={styles.baixo}></div>
-            </div>
-            <div className={styles.linknav}>
-                <h3>O MyFinances</h3>
-                <div className={styles.baixo}></div>
-            </div>
-            <div className={styles.linknav}>
-                <h3>duvidas frequentes</h3>
-                <div className={styles.baixo}></div>
-            </div>
-            <a href='#'><button className={styles.cadastro}><h3>Cadastre-se</h3></button></a>
-            <div className={styles.logomenu}></div>
+const NavbarFinances = () => {
+  return (
+    <div className={styles.nav}>
+      <a href='./localhost:3000'>
+        <div className={styles.logo}></div>
+      </a>
+      <div className={`${styles.linknav} ${styles.dropdown}`}>
+        <h3>Para Você</h3>
+        <div className={styles.baixo}></div>
+        <div className={`${styles.dropdownContent}`}>
+          {/* Conteúdo do dropdown para "Para Você" */}
+          <p>Conteúdo do dropdown para Para Você.</p>
         </div>
-    );
+      </div>
+      <div className={`${styles.linknav} ${styles.dropdown}`}>
+        <h3>Para seu Negócio</h3>
+        <div className={styles.baixo}></div>
+        <div className={`${styles.dropdownContent}`}>
+          {/* Conteúdo do dropdown para "Para seu Negócio" */}
+          <p>Conteúdo do dropdown para Para seu Negócio.</p>
+        </div>
+      </div>
+      <div className={`${styles.linknav} ${styles.dropdown}`}>
+        <h3>O MyFinances</h3>
+        <div className={styles.baixo}></div>
+        <div className={`${styles.dropdownContent}`}>
+          {/* Conteúdo do dropdown para "O MyFinances" */}
+          <p>Conteúdo do dropdown para O MyFinances.</p>
+        </div>
+      </div>
+      <div className={`${styles.linknav} ${styles.dropdown}`}>
+        <h3>Dúvidas Frequentes</h3>
+        <div className={styles.baixo}></div>
+        <div className={`${styles.dropdownContent}`}>
+          {/* Conteúdo do dropdown para "Dúvidas Frequentes" */}
+          <p>Conteúdo do dropdown para Dúvidas Frequentes.</p>
+        </div>
+      </div>
+      <button className={styles.cadastro}>
+        <a href='oi'>
+          <h3>Cadastre-se</h3>
+        </a>
+      </button>
+      <div className={styles.logomenu}></div>
+    </div>
+  );
 }
 
 export default NavbarFinances;
